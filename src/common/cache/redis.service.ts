e2e4f80fc6387ed35ce53bc753bac105ae9b3929
alikilicw <alikilic.w1@gmail.com>
2024-12-25 00:04:53 +0300

    cleaned redis service methods

--- src/common/cache/redis.service.ts
+++ src/common/cache/redis.service.ts
@@ -6,39 +6,12 @@ import { Redis } from 'ioredis'
 export class RedisService {
     constructor(@InjectRedis() private readonly redis: Redis) {}
 
-    async set(key: string, value: string, expireSeconds?: number): Promise<string> {
-        if (expireSeconds) {
-            return await this.redis.set(key, value, 'EX', expireSeconds)
-        }
-        return await this.redis.set(key, value)
-    }
-
-    async get(key: string): Promise<string | null> {
-        return await this.redis.get(key)
-    }
-
-    async del(key: string): Promise<number> {
-        return await this.redis.del(key)
-    }
-
-    async keys(pattern: string): Promise<string[]> {
-        return await this.redis.keys(pattern)
-    }
-
     async hset(hash: string, field: string, value: string | number): Promise<number> {
         return await this.redis.hset(hash, field, value)
     }
 
     async hget(hash: string, field: string): Promise<string | null> {
         return await this.redis.hget(hash, field)
-    }
-
-    async hdel(hash: string, field: string): Promise<number> {
-        return await this.redis.hdel(hash, field)
-    }
-
-    async hgetall(hash: string): Promise<Record<string, string>> {
-        return await this.redis.hgetall(hash)
     }
 
     async zadd(set: string, score: number, member: string): Promise<number> {
@@ -63,14 +36,6 @@ export class RedisService {
     async zscore(set: string, member: string): Promise<number | null> {
         const score = await this.redis.zscore(set, member)
         return score ? Number(score) : null
-    }
-
-    async expire(key: string, seconds: number): Promise<number> {
-        return await this.redis.expire(key, seconds)
-    }
-
-    async ttl(key: string): Promise<number> {
-        return await this.redis.ttl(key)
     }
 
     async zcard(key: string): Promise<number> {
